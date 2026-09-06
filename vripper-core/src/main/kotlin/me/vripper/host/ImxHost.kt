4c17f2fd59bbf291ba11077a9e27ded7e8dc9f3d
dev-claw <53543762+dev-claw@users.noreply.github.com>
2026-09-06 22:21:44 +0100

    fixes #322

--- vripper-core/src/main/kotlin/me/vripper/host/ImxHost.kt
+++ vripper-core/src/main/kotlin/me/vripper/host/ImxHost.kt
@@ -17,35 +17,36 @@ internal class ImxHost : Host("imx.to", 8) {
 
     private val log by LoggerDelegate()
 
+    data class ImxLink(val pageUrl: String, val thumbUrl: String, val imageUrl: String)
+
     @Throws(HostException::class)
     override fun resolve(
         context: ImageDownloadRunnable.Context
     ): Pair<String, String> {
         log.debug("Resolving name and image url for ${context.imageEntity.url}")
-        val imgTitle = getTitle(context)
-        val imgUrl = findPattern(context.imageEntity)
+        val imxLink = findPattern(context.imageEntity)
+        val imgTitle = getTitle(imxLink.pageUrl, context)
         return Pair(
-            imgTitle, imgUrl
+            imgTitle, imxLink.imageUrl
         )
     }
 
-    private fun getTitle(context: ImageDownloadRunnable.Context): String {
+    private fun getTitle(pageUrl: String, context: ImageDownloadRunnable.Context): String {
 
         return if (context.settings.hostSettings[HostName.IMX]?.get(HostSettingKey.TRY_TO_FETCH_ORIGINAL_FILENAME)
                 .toBoolean()
         ) {
-            val httpsUrl = context.imageEntity.url.replace("http:", "https:")
-            val document = fetchDocument(httpsUrl, context)
+            val document = fetchDocument(pageUrl, context)
             var value: String? = null
-            log.debug("Looking for xpath expression $CONTINUE_BUTTON_XPATH in $httpsUrl")
+            log.debug("Looking for xpath expression $CONTINUE_BUTTON_XPATH in $pageUrl")
             val contDiv = XpathUtils.getAsNode(document, CONTINUE_BUTTON_XPATH)
                 ?: throw HostException("$CONTINUE_BUTTON_XPATH cannot be found")
             val node = contDiv.attributes.getNamedItem("value")
             if (node != null) {
                 value = node.textContent
             }
-            log.debug("Click button found for $httpsUrl")
-            val httpPost: HttpPost = HttpPost(httpsUrl).also {
+            log.debug("Click button found for $pageUrl")
+            val httpPost: HttpPost = HttpPost(pageUrl).also {
                 it.entity = UrlEncodedFormEntity(listOf(BasicNameValuePair("imgContinue", value)))
             }.also { context.requests.add(it) }
             log.debug("Requesting {}", httpPost)
@@ -56,10 +57,10 @@ internal class ImxHost : Host("imx.to", 8) {
                 HtmlUtils.clean(response.entity.content)
             }
 
-            log.debug("Looking for xpath expression $IMG_XPATH in $httpsUrl")
+            log.debug("Looking for xpath expression $IMG_XPATH in $pageUrl")
             val imgNode = XpathUtils.getAsNode(doc, IMG_XPATH)
 
-            log.debug("Resolving name for $httpsUrl")
+            log.debug("Resolving name for $pageUrl")
             val imgTitle = imgNode?.attributes?.getNamedItem("alt")?.textContent?.trim() ?: ""
             imgTitle
 
@@ -68,24 +69,52 @@ internal class ImxHost : Host("imx.to", 8) {
         }
     }
 
-    private fun findPattern(image: ImageEntity): String {
+    private fun findPattern(image: ImageEntity): ImxLink {
         val url = image.thumbUrl
             .replace("http:", "https:")
         return if (url.startsWith("https://image.imx.to/u/t/")) {
-            "https://image.imx.to/u/i/" + url.replace("https://image.imx.to/u/t/", "")
+            ImxLink(
+                "https://imx.to/i/" + extractIdFromUrl(url),
+                url,
+                "https://image.imx.to/u/i/" + url.replace("https://image.imx.to/u/t/", "")
+            )
         } else if (url.startsWith("https://imx.to/u/t")) {
-            "https://image.imx.to/u/i/" + url.replace("https://imx.to/u/t", "")
+            ImxLink(
+                "https://imx.to/i/" + extractIdFromUrl(url),
+                url,
+                "https://image.imx.to/u/i/" + url.replace("https://imx.to/u/t", "")
+            )
         } else if (url.startsWith("https://t.imx.to/t/")) {
-            "https://image.imx.to/u/i/" + url.replace("https://t.imx.to/t/", "")
+            ImxLink(
+                "https://imx.to/i/" + extractIdFromUrl(url),
+                url,
+                "https://image.imx.to/u/i/" + url.replace("https://t.imx.to/t/", "")
+            )
         } else if (url.startsWith("https://imx.to/upload/small/")) {
-            "https://image.imx.to/u/i/" + url.replace("https://imx.to/upload/small/", "")
+            ImxLink(
+                "https://imx.to/i/" + extractIdFromUrl(url),
+                url,
+                "https://image.imx.to/u/i/" + url.replace("https://imx.to/upload/small/", "")
+            )
         } else if (url.startsWith("https://i.imx.to/t/")) {
-            "https://image.imx.to/u/i/" + url.replace("https://i.imx.to/t/", "")
+            ImxLink(
+                "https://imx.to/i/" + extractIdFromUrl(url),
+                url,
+                "https://image.imx.to/u/i/" + url.replace("https://i.imx.to/t/", "")
+            )
         } else if (url.startsWith("https://image.imx.to/u/i/")) {
-            url
+            ImxLink("https://imx.to/i/" + extractIdFromUrl(url), url.replace("/u/i", "/u/t"), url)
         } else {
             throw HostException("Cannot find pattern for url ${image.thumbUrl}")
         }
+    }
+
+    fun extractIdFromUrl(url: String): String? {
+        // Regex matches the string between the last slash '/' and the dot '.' before the extension
+        val regex = """/([^/\s]+)\.[a-zA-Z0-9]+$""".toRegex()
+        val matchResult = regex.find(url)
+
+        return matchResult?.groupValues?.get(1)
     }
 
     companion object {
